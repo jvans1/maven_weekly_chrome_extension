8e300a6bbe9f0798b7115ad02234d6bec4f3ae3b
James Vanneman <jvannem@gmail.com>
2016-04-19 08:23:11 -0400

    Change extensions size

--- ext/add_link.js
+++ ext/add_link.js
@@ -1,25 +1,10 @@
-
-console.log('registering');
-chrome.runtime.onMessageExternal.addListener(function(message, sender, fn){
-  console.log("external message");
-  console.log("message received", message);
-  fn({ hi: "butt" } );
+chrome.runtime.onConnectExternal.addListener(function(port){
+  port.onMessage.addListener(function(formObject){
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
+      var activeUrl = tabs[0].url;
+      formObject.link = activeUrl;
+      port.postMessage(formObject);
+    });
+  });
 });
 
-chrome.runtime.onMessage.addListener(function(message, sender, fn){
-  console.log("regular message");
-  console.log("message received", message);
-  fn({ hi: "butt" } );
-});
-// chrome.runtime.onConnectExternal.addListener(function(port){
-  // console.log("external connection set")
-  // port.onMessage.addListener(function(formObject){
-    // console.log("listnere fired")
-    // chrome.tabs.query({ active: true, currentWindow: true }, function(tabs){
-      // var activeUrl = tabs[0].url;
-      // formObject.link = activeUrl;
-    // console.log("posting message back", formObject)
-      // port.postMessage(formObject);
-    // });
-  // });
-// });
